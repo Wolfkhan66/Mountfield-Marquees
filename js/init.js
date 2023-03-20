c5c62f304b6ad5d170f4481c03ba6c3b3849885e
Cai Lehwald <cailehwald@googlemail.com>
2023-03-20 20:19:23 +0000

    Add toggle buttons to the carousel

--- js/init.js
+++ js/init.js
@@ -4,8 +4,7 @@
     $('.sidenav').sidenav();
       $('.parallax').parallax();
       $('.carousel.carousel-slider').carousel({
-          fullWidth: true,
-          indicators: true      });
+          fullWidth: true    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
@@ -16,3 +15,15 @@ const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+
+function toggleSlide(direction) {
+    var instance = M.Carousel.getInstance($('.carousel'));
+
+    if (direction == "left") {
+        instance.prev(); 
+    } else {
+        instance.next(); 
+
+    }
+}
