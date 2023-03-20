e5590250edb623e42ca8b2780f27051d2ad3d1f0
Cai Lehwald <cailehwald@googlemail.com>
2023-03-20 19:31:57 +0000

    Initial commit & mockup
    Roughly mock the layout and content of the site

---
+++ js/init.js
@@ -0,0 +1,18 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+      $('.parallax').parallax();
+      $('.carousel.carousel-slider').carousel({
+          fullWidth: true,
+          indicators: true      });
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+const map = L.map('map').setView([51.505, -0.09], 13);
+
+const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
